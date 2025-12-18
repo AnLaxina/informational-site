ddececcd5a712aa2867a6385d2501e399ff195de
AnLaxina <anilovlaxina@gmail.com>
2025-12-18 09:19:49 -0800

    feat: add basic routing for error page and home page

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,23 @@
-console.log(`The URL is: ${URL}`);
+import http from "node:http";
+import fs from "node:fs/promises";
+
+const server = http.createServer(async (request, response) => {
+    try {
+        if (request.url === "/") {
+            const htmlContent = await fs.readFile("./index.html");
+            response.writeHead(200, { "Content-Type": "text/html" });
+            response.end(htmlContent);
+        } else {
+            // Error Page
+            const htmlContent = await fs.readFile("./pages/404.html");
+            response.writeHead(404, { "Content-Type": "text/html" });
+            response.end(htmlContent);
+        }
+    } catch (error) {
+        console.log(`Here is the error: ${err}`);
+        response.writeHead(500);
+        response.end();
+    }
+});
+
+server.listen(8080);
